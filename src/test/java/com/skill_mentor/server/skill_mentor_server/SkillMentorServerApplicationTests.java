ab9dcd160f37d30bac14c3d2ddb8c0c4067bab32
Pamod Jayaneth <pamodjayaneth@gmail.com>
2025-07-04 21:10:47 +0530

    init commit

---
+++ src/test/java/com/skill_mentor/server/skill_mentor_server/SkillMentorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skill_mentor.server.skill_mentor_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillMentorServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
