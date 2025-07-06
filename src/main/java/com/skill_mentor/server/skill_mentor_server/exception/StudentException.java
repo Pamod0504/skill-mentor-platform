d50fb621e5d051818d5e26c05a7bdb5bea2054a9
Pamod Jayaneth <pamodjayaneth@gmail.com>
2025-07-07 02:24:43 +0530

    create student entity/DTO/service

---
+++ src/main/java/com/skill_mentor/server/skill_mentor_server/exception/StudentException.java
@@ -0,0 +1,7 @@
+package com.skill_mentor.server.skill_mentor_server.exception;
+
+public class StudentException extends RuntimeException{
+    public StudentException(String message){
+        super(message);
+    }
+}
