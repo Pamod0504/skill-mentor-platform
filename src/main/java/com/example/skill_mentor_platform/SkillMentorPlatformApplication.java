237c2a07d939f560b3d07b0f3ae1a3df3e2f4b5c
Pamod Jayaneth <pamodjayaneth@gmail.com>
2025-07-30 10:19:40 +0530

    Add files via upload

---
+++ src/main/java/com/example/skill_mentor_platform/SkillMentorPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.skill_mentor_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillMentorPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillMentorPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skill_mentor_platform/SkillMentorPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skill_mentor_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillMentorPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
